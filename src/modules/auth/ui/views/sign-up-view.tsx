23593eb887b3afe35d52533a9903f25854922093
ishan-codes-code <srivastava.ishan.work@gmail.com>
2025-12-14 21:14:33 +0530

    authentiation socials setup

--- src/modules/auth/ui/views/sign-up-view.tsx
+++ src/modules/auth/ui/views/sign-up-view.tsx
@@ -16,7 +16,8 @@ import { Alert, AlertTitle } from '@/components/ui/alert';
 import { Card, CardContent } from '@/components/ui/card';
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 
-import { OctagonAlertIcon } from 'lucide-react';
+import { OctagonAlertIcon, } from 'lucide-react';
+import { FaGithub, FaGoogle } from 'react-icons/fa'
 
 const formSchema = z
     .object({
@@ -38,6 +39,7 @@ const SignUpView = () => {
 
     const router = useRouter();
     const [error, setError] = useState<string | null>(null);
+    const [loading, setLoading] = useState(false);
 
 
     const form = useForm<z.infer<typeof formSchema>>({
@@ -57,10 +59,11 @@ const SignUpView = () => {
         await authClient.signUp.email({
             name: data.name,
             email: data.email,
-            password: data.password
+            password: data.password,
+            callbackURL: '/',
         }, {
             onSuccess: () => {
-                router.push("/");
+                router.push('/')
 
             },
             onError: ({ error }) => {
@@ -69,6 +72,27 @@ const SignUpView = () => {
         })
     }
 
+
+    // callbackURL: '/' - 'for now not woring'
+    const onSocials = async (provider: 'github' | 'google') => {
+        setLoading(true)
+        setError(null)
+
+        await authClient.signIn.social({
+            provider: provider,
+
+        }, {
+            onSuccess: () => {
+
+                setLoading(false)
+
+            },
+            onError: ({ error }) => {
+                setError(error.message);
+                setLoading(false)
+            },
+        })
+    }
 
     return (
         <div className="flex flex-col gap-6">
@@ -88,7 +112,7 @@ const SignUpView = () => {
                                         <FormItem>
                                             <FormLabel> Name</FormLabel>
                                             <FormControl>
-                                                <Input placeholder='x@emaple.com' type='text' {...field} />
+                                                <Input placeholder='Rob Stark' type='text' {...field} />
                                             </FormControl>
                                             <FormMessage />
                                         </FormItem>
@@ -98,7 +122,7 @@ const SignUpView = () => {
                                         <FormItem>
                                             <FormLabel> Email</FormLabel>
                                             <FormControl>
-                                                <Input placeholder='x@emaple.com' type='email' {...field} />
+                                                <Input placeholder='x@example.com' type='email' {...field} />
                                             </FormControl>
                                             <FormMessage />
                                         </FormItem>
@@ -136,7 +160,7 @@ const SignUpView = () => {
                                 )}
                                 <Button
                                     type="submit"
-                                    disabled={form.formState.isSubmitting}
+                                    disabled={form.formState.isSubmitting || loading}
                                     className="w-full"
                                 >
                                     Sign Up
@@ -146,8 +170,12 @@ const SignUpView = () => {
                                     <span className='bg-card text-muted-foreground relative z-10 px-2'>Or continue with</span>
                                 </div>
                                 <div className='grid grid-cols-2 gap-4'>
-                                    <Button variant="outline" type='button' className='w-full'>Google</Button>
-                                    <Button variant="outline" type='button' className='w-full'>Github</Button>
+                                    <Button disabled={form.formState.isSubmitting || loading} variant="outline" type='button' className='w-full cursor-pointer' onClick={() => onSocials('google')}>
+                                        <FaGoogle />
+                                    </Button>
+                                    <Button disabled={form.formState.isSubmitting || loading} variant="outline" type='button' className='w-full cursor-pointer' onClick={() => onSocials('github')}>
+                                        <FaGithub />
+                                    </Button>
 
                                 </div>
                                 <div className='text-center text-sm'>Already have an account <Link href="/sign-in" className='underline underline-offset-4'>Sign in</Link></div>
@@ -160,7 +188,7 @@ const SignUpView = () => {
                         <span className='p-2 h-24 w-24 flex items-center justify-center rounded-full bg-black'>
                             <img className='h-20' src="./logo.svg" alt="logo" />
                         </span>
-                        <p className='text-2xl font-semibold text-white'>Meet Ai</p>
+                        <p className='text-2xl font-semibold text-white'>Echo Ai</p>
                     </div>
                 </CardContent>
             </Card>
