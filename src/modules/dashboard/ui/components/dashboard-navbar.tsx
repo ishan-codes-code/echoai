4fd8793bf9eea3638238056349d88ea249f1fb49
ishan-codes-code <srivastava.ishan.work@gmail.com>
2025-12-15 18:09:08 +0530

    adding responsive dialog

--- src/modules/dashboard/ui/components/dashboard-navbar.tsx
+++ src/modules/dashboard/ui/components/dashboard-navbar.tsx
@@ -29,7 +29,7 @@ const DashboardNavbar = () => {
         <>
             <DashboardCommand open={commandOpen} setOpen={setCommandOpen} />
             <nav className="flex px-4 gap-x-2 items-center py-3 border-b bg-background ">
-                <Button className="size-9 cursor-pointer" variant="outline" onClick={toggleSidebar}>
+                <Button className="size-9" variant="outline" onClick={toggleSidebar}>
                     {(state === 'collapsed' || isMobile)
                         ? <PanelLeftIcon className="size-4" />
                         : <PanelLeftCloseIcon className="size-4" />
