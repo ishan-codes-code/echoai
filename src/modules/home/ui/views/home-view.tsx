e2670c0c2350f023272a20982ebdd11c23def256
ishan-codes-code <srivastava.ishan.work@gmail.com>
2025-12-15 17:01:00 +0530

    agents setup

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,17 +1,14 @@
 'use client'
 
-import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
 
 
 
 const HomeView = () => {
-    const trpc = useTRPC()
-    const { data } = useQuery(trpc.hello.queryOptions({ text: 'Antonio' }))
 
     return (
         <div className="p-4 m-auto mt-22 max-w-xl">
-            {data?.greeting}
+            Home view
+
         </div>
     )
 
