398062309613b726dbdd364558db7b5a944628de
ishan-codes-code <srivastava.ishan.work@gmail.com>
2025-12-16 22:34:00 +0530

    agents filters setup

--- src/modules/agents/ui/components/columns.tsx
+++ src/modules/agents/ui/components/columns.tsx
@@ -38,8 +38,8 @@ export const columns: ColumnDef<AgentGetOne>[] = [
         cell: ({ row }) => (
             <Badge variant={"outline"} className="flex items-center gap-x-2 [&>svg]:size-4">
                 <VideoIcon className="text-blue-700" />
-                {/* {row.original.meetingsCount} {row.original.meetingsCount === 1 ? "meeting" : "meetings"} */}
-                6 meetings
+                {row.original.meetingsCount} {row.original.meetingsCount === 1 ? "meeting" : "meetings"}
+                {/* 6 meetings */}
             </Badge>
         )
     }
